dfdd165ea5fb5051f0c0519837b3d755d4c13923
Michelle AVOMO <michelle.avomo@codeworks.fr>
2023-01-27 16:24:26 +0100

    last version

--- PythonRemix/shell.nix
+++ PythonRemix/shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = [ pkgs.buildPackages.python310 pkgs.buildPackages.python310Packages.pytest];
+    packages = [ pkgs.python310 pkgs.python310Packages.pytest];
 }
+
