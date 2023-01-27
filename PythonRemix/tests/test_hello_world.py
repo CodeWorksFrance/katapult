de660faeb50727a41da85d6d73631c4a1b66f5fb
Michelle AVOMO <michelle.avomo@codeworks.fr>
2023-01-27 15:46:50 +0100

    init second python codebase

--- PythonRemix/tests/test_hello_world.py
+++ PythonRemix/tests/test_hello_world.py
@@ -3,4 +3,4 @@ from app.hello_world import HelloWorld
 
 def test_greets_the_world():
     hw = HelloWorld()
-    assert hw.greets_the_world() == "Hello, world!!!!"
+    assert hw.greets_the_world() == "Hello, world!"
